2761d0e35534acb03b84183db6938534d0676f42
jxv <joevargas@hush.com>
2014-10-07 17:22:56 -0700

    added unit1-14; added unit1-15

---
+++ unit_1_15_Multiple_Measurements.rs
@@ -0,0 +1,32 @@
+use std::iter::AdditiveIterator;
+
+#[deriving(Show,PartialEq)]
+enum Color {
+    Green,
+    Red,
+}
+
+static WORLD: [Color, ..5] = [Green, Red, Red, Green, Green];
+static P_HIT: f32 = 0.6;
+static P_MISS: f32 = 0.2;
+
+fn sense(p: &Vec<f32>, z: Color) -> Vec<f32> {
+    let mut q = p.clone();
+    for i in range(0u, q.len()) {
+        *q.get_mut(i) *= if z == WORLD[i] { P_HIT } else { P_MISS };
+    }
+    let sum = q.iter().map(|&x| x).sum();
+    for i in range(0u, q.len()) {
+        *q.get_mut(i) /= sum;
+    }
+    q
+}
+
+fn main() {
+    let mut p: Vec<f32> = vec![0.2, 0.2, 0.2, 0.2, 0.2];
+    let measurements: Vec<Color> = vec![Red, Green];
+    for &z in measurements.iter() {
+        p = sense(&p, z);
+    }
+    println!("{}", p);
+}
