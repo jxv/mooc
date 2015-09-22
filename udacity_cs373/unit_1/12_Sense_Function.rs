0afb31222f45275d081a9f3bb11756f3af801966
jxv <jxv@hush.com>
2015-09-21 18:11:36 -0700

    Changes for newer rust

--- udacity_cs373/unit_1/12_Sense_Function.rs
+++ udacity_cs373/unit_1/12_Sense_Function.rs
@@ -1,23 +1,23 @@
-#[deriving(Show,PartialEq)]
+#[derive(Debug,PartialEq)]
 enum Color {
     Green,
     Red,
 }
 
-static WORLD: [Color, ..5] = [Green, Red, Red, Green, Green];
+static WORLD: [Color;5] = [Color::Green, Color::Red, Color::Red, Color::Green, Color::Green];
 static P_HIT: f32 = 0.6;
 static P_MISS: f32 = 0.2;
 
 fn sense(p: &Vec<f32>, z: Color) -> Vec<f32> {
     let mut q = p.clone();
-    for i in range(0u, q.len()) {
-        *q.get_mut(i) *= if z == WORLD[i] { P_HIT } else { P_MISS };
+    for i in 0..q.len() {
+        q[i] *= if z == WORLD[i] { P_HIT } else { P_MISS };
     }
     q
 }
 
 fn main() {
     let p: Vec<f32> = vec![0.2, 0.2, 0.2, 0.2, 0.2];
-    let z: Color = Red;
-    println!("{}", sense(&p, z));
+    let z: Color = Color::Red;
+    println!("{:?}", sense(&p, z));
 }
--- udacity_cs373/unit_6/06_Segemented_Cte.rs
+++ udacity_cs373/unit_6/06_Segemented_Cte.rs
@@ -1,13 +1,12 @@
-#![feature(tuple_indexing)]
+extern crate rand;
 
-use std::iter::AdditiveIterator;
-use std::rand::{task_rng, random};
-use std::rand::distributions::{Normal, IndependentSample};
-use std::num::{Float, FloatMath};
-use std::num::{FromPrimitive};
+//use std::iter::AdditiveIterator;
+use rand::{rthread_rng, random};
+//use std::rand::distributions::{Normal, IndependentSample};
+//use std::num::{Float, FloatMath};
+//use std::num::{FromPrimitive};
 
-
-#[deriving(Show,PartialEq,Clone)]
+#[derive(Debug,PartialEq,Clone,Copy)]
 enum Dir {
     Up,
     Left,
@@ -15,11 +14,9 @@ enum Dir {
     Right,
 }
 
+type Grid = [[Space;6];5];
 
-#[deriving(Show,Clone)]
-type Grid = [[Space,..6],..5];
-
-static DIRS: [Dir, ..4] = [Up, Left, Down, Right];
+static DIRS: [Dir;4] = [Up, Left, Down, Right];
 static GRID: Grid = [
     [O,X,O,O,O,O],
     [O,X,O,X,X,O],
@@ -27,8 +24,8 @@ static GRID: Grid = [
     [O,O,O,X,O,X],
     [O,X,O,X,O,O]];
 
-static INIT: (uint,uint) = (0,0);
-static GOAL: (uint,uint) = (4,5);
+static INIT: (usize,usize) = (0,0);
+static GOAL: (usize,usize) = (4,5);
 
 static STEERING_NOISE: f32 = 0.1;
 static DISTANCE_NOISE: f32 = 0.03;
@@ -39,8 +36,7 @@ static WEIGHT_SMOOTH: f32 = 0.2;
 static P_GAIN: f32 = 2.0;
 static D_GAIN: f32 = 6.0;
 
-
-fn forward(grid: &Grid, (x,y): (uint, uint), dir: Dir) -> Option<(uint, uint)> {
+fn forward(grid: &Grid, (x,y): (usize, usize), dir: Dir) -> Option<(usize, usize)> {
     let (x2, y2) = match dir {
         Up    => (x, y - 1),
         Left  => (x - 1, y),
@@ -51,7 +47,7 @@ fn forward(grid: &Grid, (x,y): (uint, uint), dir: Dir) -> Option<(uint, uint)> {
 }
 
 #[allow(dead_code)]
-fn from_dir(dir: Dir) -> uint {
+fn from_dir(dir: Dir) -> usize {
     match dir { Up => 0, Left => 1, Down => 2, Right => 3 }
 }
 
@@ -60,25 +56,23 @@ fn opp_dir(dir: Dir) -> Dir {
     match dir { Up => Down, Left => Right, Down => Up, Right => Left }
 }
 
-#[deriving(Show,PartialEq,Clone)]
+#[derive(Debug,PartialEq,Clone,Copy)]
 enum Space {
     O,
     X,
 }
 
-
 struct Plan {
-    cost: uint,
+    cost: usize,
     grid: Grid,
-    init: (uint,uint),
-    goal: (uint,uint),
-    heuristic: Vec<Vec<uint>>,
-    path: Vec<Vec<uint>>,
+    init: (usize,usize),
+    goal: (usize,usize),
+    heuristic: Vec<Vec<usize>>,
+    path: Vec<Vec<usize>>,
     spath: Vec<Vec<f32>>,
 }
 
-
-#[deriving(Show,Clone)]
+#[derive(Debug,Clone,Copy)]
 struct Robot {
     x: f32,
     y: f32,
@@ -87,30 +81,24 @@ struct Robot {
     steering_noise: f32,
     distance_noise: f32,
     measurement_noise: f32,
-    num_collisions: uint,
-    num_steps: uint,
+    num_collisions: usize,
+    num_steps: usize,
 }
 
-
-#[deriving(Show,Clone)]
+#[derive(Debug,Clone,Copy)]
 struct Particles {
-    n: uint,
+    n: usize,
     steering_noise: f32,
     distance_noise: f32,
     measurement_noise: f32,
     data: Vec<Robot>,
 }
 
-
-#[deriving(Show,Clone)]
 impl Plan {
-    
-    fn new(grid: &Grid, init: (uint,uint), goal: (uint,uint)) -> Plan {
+    fn new(grid: &Grid, init: (usize,usize), goal: (usize,usize)) -> Plan {
         Plan::new_with_extra(grid, init, goal, 1)
     }
-
-
-    fn new_with_extra(grid: &Grid, init: (uint,uint), goal: (uint,uint), cost: uint) -> Plan {
+    fn new_with_extra(grid: &Grid, init: (usize,usize), goal: (usize,usize), cost: usize) -> Plan {
         Plan {
             cost: cost,
             grid:[[grid[0][0], grid[0][1], grid[0][2], grid[0][3], grid[0][4], grid[0][5]],
@@ -125,30 +113,24 @@ impl Plan {
             spath: Vec::new(),
         }
     }
-
-
-    fn make_heuristic(grid: &Grid, goal: (uint,uint), /*cost*/ _: uint) -> Vec<Vec<uint>> {
-        Vec::from_fn(grid.len(), |i: uint| {
-            Vec::from_fn(grid[i].len(), |j: uint| {
+    fn make_heuristic(grid: &Grid, goal: (usize,usize), /*cost*/ _: usize) -> Vec<Vec<usize>> {
+        Vec::from_fn(grid.len(), |i: usize| {
+            Vec::from_fn(grid[i].len(), |j: usize| {
                 (if i < goal.0 { goal.0 - i } else { i - goal.0 }) +
                 (if j < goal.1 { goal.1 - j } else { j - goal.1 })
             })
         })
     }
-
-
     fn astar(&mut self) {
         if self.heuristic.len() == 0 {
             panic!("Heuristic must be defined to run astar");
         }
-        let mut closed =
-            Vec::from_elem(self.grid.len(), Vec::from_elem(self.grid[0].len(), false));
-        let mut action: Vec<Vec<Option<Dir>>> =
-            Vec::from_elem(self.grid.len(), Vec::from_elem(self.grid[0].len(), None));
-        closed[self.init.0 as uint][self.init.1] = true;
+        let mut closed = vec![vec![false; self.grid[0].len()]; self.grid.len()];
+        let mut action: Vec<Vec<Option<Dir>>> = vec![vec![None; self.grid[0].len()]; self.grid.len()];
+        closed[self.init.0 as usize][self.init.1] = true;
         let x = self.init.0;
         let y = self.init.1;
-        let h = self.heuristic[x as uint][y as uint];
+        let h = self.heuristic[x as usize][y as usize];
         let g = 0;
         let f = g + h;
         let mut open = vec![(f, g, h, x, y)];
@@ -202,33 +184,29 @@ impl Plan {
             invpath.push([x,y]);
         }
         self.path = Vec::with_capacity(invpath.len());
-        for i in range(0u,invpath.len()) {
+        for i in 0..invpath.len() {
             let e = invpath[invpath.len() - 1 - i];
             self.path.push(vec![e[0],e[1]]);
         }
     }
-
-
 #[allow(dead_code)]
     fn smooth(&mut self) {
         self.smooth_extra(0.1, 0.1, 0.000001)
     }
-
-
     fn smooth_extra(&mut self, weight_data: f32, weight_smooth: f32, tolerance: f32) {
         if self.path.len() == 0 {
             panic!("run astar before smoothing path");
         }
-        self.spath = Vec::from_fn(self.path.len(), |i: uint| {
-            Vec::from_fn(self.path[i].len(), |j: uint| {
+        self.spath = Vec::from_fn(self.path.len(), |i: usize| {
+            Vec::from_fn(self.path[i].len(), |j: usize| {
                 self.path[i][j] as f32
             })
         });
         let mut change = tolerance;
         while change >= tolerance {
             change = 0.0;
-            for i in range(1u, self.path.len() - 1) {
-                for j in range(0u, self.path[0].len()) {
+            for i in 1..(self.path.len() - 1) {
+                for j in 0..self.path[0].len() {
                     let org  = self.path[i][j] as f32;
                     let curr = self.spath[i][j];
                     let prev = self.spath[i-1][j];
@@ -252,14 +230,10 @@ impl Plan {
     }
 }
 
-
 impl Robot {
-
     fn new() -> Robot {
         Robot::new_with_extra(0.5)
     }
-
-
     fn new_with_extra(length: f32) -> Robot {
         Robot {
             x: 0.0,
@@ -273,25 +247,19 @@ impl Robot {
             num_steps: 0,
         }
     }
-
-
     fn set(&mut self, new_x: f32, new_y: f32, new_orientation: f32) {
         self.x = new_x;
         self.y = new_y;
         self.orientation = modulo(new_orientation, Float::two_pi());
     }
-
-
     fn set_noise(&mut self, new_s_noise: f32, new_d_noise: f32, new_m_noise: f32) {
         self.steering_noise = new_s_noise;
         self.distance_noise = new_d_noise;
         self.measurement_noise = new_m_noise;
     }
-
-
     fn check_collision(&mut self, grid: &Grid) -> bool {
-        for i in range(0u, grid.len()) {
-            for j in range(0u, grid[i].len()) {
+        for i in 0..grid.len() {
+            for j in 0..grid[i].len() {
                 if grid[i][j] == X {
                     let (i,j) = (i as f32, j as f32);
                     let dist = ((self.x - i).powi(2) + (self.y - j).powi(2)).sqrt();
@@ -304,28 +272,18 @@ impl Robot {
         }
         true
     }
-
-
-    fn check_goal(&self, goal: (uint,uint)) -> bool {
+    fn check_goal(&self, goal: (usize,usize)) -> bool {
         self.check_goal_extra(goal, 1.0)
     }
-
-    
-    fn check_goal_extra(&self, goal: (uint,uint), threshold: f32) -> bool {
+    fn check_goal_extra(&self, goal: (usize,usize), threshold: f32) -> bool {
         let dist = ( ((goal.0 as f32) - self.x).powi(2)
                    + ((goal.1 as f32) - self.y).powi(2)).sqrt();
         dist < threshold
     }
-
-
     //--
-
-
     fn travel(&self, grid: &Grid, steering: f32, distance: f32) -> Robot {
         self.travel_extra(grid, steering, distance, 0.001, Float::frac_pi_4())
     }
-
-
     fn travel_extra(&self, _: &Grid, steering: f32, distance: f32, tolerance: f32,
             max_steering_angle: f32) -> Robot {
         let steering =
@@ -362,13 +320,9 @@ impl Robot {
 
         res
     }
-
-
     fn sense(&self) -> (f32, f32) {
         (gauss(self.x, self.measurement_noise), gauss(self.y, self.measurement_noise))
     }
-
-   
     fn measurement_prob(&self, measurement: (f32, f32)) -> f32 {
         let error_x = measurement.0 - self.x;
         let error_y = measurement.1 - self.y;
@@ -378,45 +332,36 @@ impl Robot {
                       / (self.measurement_noise.powi(2) * Float::two_pi()).sqrt();
         error
     }
-    
-    
     fn print(&self) {
-        print!("[x={:.5f} y={:.5f} orient={:.5f}]", self.x, self.y, self.orientation);
+        print!("[x={:.5} y={:.5} orient={:.5}]", self.x, self.y, self.orientation);
     }
-   
 }
 
-
 impl Particles {
-
     fn new(x: f32, y: f32, theta: f32, steering_noise: f32, distance_noise: f32,
             measurement_noise: f32) -> Particles {
         Particles::new_extra(x, y, theta, steering_noise, distance_noise, measurement_noise, 100)
     }
-
-
     fn new_extra(x: f32, y: f32, theta: f32, steering_noise: f32, distance_noise: f32,
-            measurement_noise: f32, n: uint) -> Particles {
+            measurement_noise: f32, n: usize) -> Particles {
         Particles {
             n: n,
             steering_noise: steering_noise,
             distance_noise: distance_noise,
             measurement_noise: measurement_noise,
-            data: Vec::from_elem(n, {
+            data: vec![{
                     let mut r = Robot::new();
                     r.set(x, y, theta);
                     r.set_noise(steering_noise, distance_noise, measurement_noise);
                     r
-                }),
+                }, n],
         }
     }
-
-
     fn get_position(&self) -> (f32, f32, f32) {
         let mut x = 0.0;
         let mut y = 0.0;
         let mut orientation = 0.0;
-        for i in range(0u, self.n) {
+        for i in 0..self.n {
             x += self.data[i].x;
             y += self.data[i].y;
             orientation += modulo(self.data[i].orientation
@@ -428,25 +373,21 @@ impl Particles {
         let n: f32 = self.n as f32;
         (x / n, y / n, orientation / n)
     }
-
-
     fn travel(&mut self, grid: &Grid, steer: f32, speed: f32) {
-        self.data = Vec::from_fn(self.data.len(), |i: uint| {
+        self.data = Vec::from_fn(self.data.len(), |i: usize| {
             self.data[i].travel(grid, steer, speed)
         });
     }
-
-
     fn sense(&mut self, z: (f32, f32)) {
-        let ws = Vec::from_fn(self.n, |i: uint| {
+        let ws = Vec::from_fn(self.n, |i: usize| {
                 self.data[i].measurement_prob(z)
             });
         let mut p3 = Vec::with_capacity(self.n);
-        let mut idx = random::<uint>() % self.n;
+        let mut idx = random::<usize>() % self.n;
         let mut beta = 0.0;
         let mut mw = 0.0;
         for &w in ws.iter() { if w > mw { mw = w; } }
-        for _ in range(0u, self.n) {
+        for _ in 0..self.n {
             beta += random::<f32>() * 2.0 * mw;
             while beta > ws[idx] {
                 beta -= ws[idx];
@@ -456,18 +397,15 @@ impl Particles {
         }
         self.data = p3;
     }
-
 }
 
-
-fn run(grid: &Grid, goal: (uint,uint), spath: &Vec<Vec<f32>>, params: (f32,f32))
-        -> (bool, uint, uint) {
+fn run(grid: &Grid, goal: (usize,usize), spath: &Vec<Vec<f32>>, params: (f32,f32))
+        -> (bool, usize, usize) {
     run_extra(grid, goal, spath, params, false, 0.1, 1000)
 }
 
-
-fn run_extra(grid: &Grid, goal: (uint,uint), spath: &Vec<Vec<f32>>, params: (f32,f32),
-        print_flag: bool, speed: f32, timeout: uint) -> (bool, uint, uint) {
+fn run_extra(grid: &Grid, goal: (usize,usize), spath: &Vec<Vec<f32>>, params: (f32,f32),
+        print_flag: bool, speed: f32, timeout: usize) -> (bool, usize, usize) {
     let mut myrobot = Robot::new();
     myrobot.set(0.0, 0.0, 0.0);
     myrobot.set_noise(STEERING_NOISE, DISTANCE_NOISE, MEASUREMENT_NOISE);
@@ -508,7 +446,6 @@ fn run_extra(grid: &Grid, goal: (uint,uint), spath: &Vec<Vec<f32>>, params: (f32
     (myrobot.check_goal(goal), myrobot.num_collisions, myrobot.num_steps)
 }
 
-
 fn main() {
     println!("{}",
             main_fn(&GRID, INIT, GOAL,
@@ -516,14 +453,13 @@ fn main() {
                     WEIGHT_DATA, WEIGHT_SMOOTH, P_GAIN, D_GAIN))
 }
 
-
-fn main_fn(grid: &Grid, init: (uint, uint), goal: (uint, uint),
+fn main_fn(grid: &Grid, init: (usize, usize), goal: (usize, usize),
         _: f32, _: f32, _: f32,
-        weight_data: f32, weight_smooth: f32, p_gain: f32, d_gain: f32) -> (bool, uint, uint) {
+        weight_data: f32, weight_smooth: f32, p_gain: f32, d_gain: f32) -> (bool, usize, usize) {
     let mut plan = Plan::new(grid, init, goal);
     plan.astar();
     plan.smooth_extra(weight_data, weight_smooth, 0.000001);
-    for i in range(0,plan.spath.len()) {
+    for i in 0..plan.spath.len() {
         println!("{} -> {}", plan.path[i], plan.spath[i]);
     }
     run(grid, goal, &plan.spath, (p_gain, d_gain))
@@ -536,7 +472,7 @@ fn twiddle(init_params: &Vec<f32>) -> Vec<f32> {
     let mut params = init_params.clone();
     let k = 10;
     let mut best_error = 0.0;
-    for _ in range(0u, k) {
+    for _ in 0..k {
         let ret = main_fn(&GRID, INIT, GOAL,
                 STEERING_NOISE, DISTANCE_NOISE, MEASUREMENT_NOISE,
                 params[0], params[1], params[2], params[3]);
@@ -544,12 +480,12 @@ fn twiddle(init_params: &Vec<f32>) -> Vec<f32> {
     }
     best_error /= k as f32;
     println!("{}", best_error);
-    let mut n: uint = 0;
+    let mut n: usize = 0;
     while dparams.iter().map(|&x| x).sum() >  0.0000001 {
-        for i in range(0u, params.len()) {
+        for i in 0..params.len() {
             params[i] += dparams[i];
             let mut err = 0.0 as f32;
-            for j in range(0u,k) {
+            for j in 0..k {
                 let ret = main_fn(&GRID, INIT, GOAL,
                                 STEERING_NOISE, DISTANCE_NOISE, MEASUREMENT_NOISE,
                                 params[0], params[1], params[2], params[3]);
@@ -562,7 +498,7 @@ fn twiddle(init_params: &Vec<f32>) -> Vec<f32> {
             } else {
                 params[i] -= 2.0 * dparams[i];
                 err = 0.0;
-                for j in range(0u,k) {
+                for j in 0..k {
                     // here
                 }
                 println!("{}", err / k as f32);
@@ -582,16 +518,14 @@ fn twiddle(init_params: &Vec<f32>) -> Vec<f32> {
     params
 }
 
-
 fn modulo(n: f32, m: f32) -> f32 {
     let mut k = n % m;
     while k < 0.0 { k += m };
     k
 }
 
-
 fn gauss(mean: f32, covar2: f32) -> f32 {
     let cvt_covar2: f64 = FromPrimitive::from_f32(covar2).unwrap();
     let cvt_mean: f64 = FromPrimitive::from_f32(mean).unwrap();
-    FromPrimitive::from_f64(Normal::new(cvt_mean, cvt_covar2).ind_sample(&mut task_rng())).unwrap()
+    FromPrimitive::from_f64(Normal::new(cvt_mean, cvt_covar2).ind_sample(&mut thread_rng())).unwrap()
 }
